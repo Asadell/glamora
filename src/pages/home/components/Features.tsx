6ddeb430b78b29218489ad4fb28aa73ee08034cd
Asadell <m.satrioasadell@gmail.com>
2024-09-30 00:49:36 +0700

    chore: update parameter value

--- src/pages/home/components/Features.tsx
+++ src/pages/home/components/Features.tsx
@@ -9,7 +9,7 @@ const Features = () => {
       <div className="grid gap-4 grid-cols-1 lg:grid-cols-2 relative">
         <img src={starFeature} alt="Star" className="w-[100px] h-[100px] absolute -top-10 -left-10" />
         {FEATURE_BERANDA_LIST.map((feature) => (
-          <FeatureBox isFeature={feature.isFeature} title={feature.title} text={feature.text} image={feature.image}/>
+          <FeatureBox  url={feature.url} isFeature={feature.isFeature} title={feature.title} text={feature.text} image={feature.image}/>
         ))}
         <img src={starFeature} alt="Star" className="w-[100px] h-[100px] absolute -bottom-10 -right-10" />
       </div>
